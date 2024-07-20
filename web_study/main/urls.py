62ffbfc447b5058a049a0a86c1b076d29bb9fd9d
Son <suruna1026@gmail.com>
2024-07-21 05:26:01 +0900

    글작성,글목록 수정본

--- web_study/main/urls.py
+++ web_study/main/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('blog/', views.blog, name="blog"),
-    path('blog/<int:pk>/', views.posting, name="posting"),
+    
+    # 수정
+    path('blog/<int:post_id>/', views.posting, name="posting"),
+    
     path('blog/new_post/', views.new_post, name='new_post'),
     ]
--- web_study/main/views.py
+++ web_study/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 # View에 Model(Post 게시글) 가져오기
 from .models import Post
 
@@ -15,18 +15,30 @@ def blog(request):
     return render(request, 'blog.html', {'postlist': postlist})
 
 # blog의 게시글(posting)을 부르는 posting 함수
-def posting(request, pk):
+#수정
+def posting(request, post_id):
     
     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)을 검색
-    post = Post.objects.get(pk=pk)
+    
+    #수정
+    # post=get_object_or_404(Post, pk=post_id)
+    post = Post.objects.get(pk=post_id)
     
     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져온다.
     return render(request, 'posting.html', {'post': post})
 
+# 수정
 def new_post(request):
     if request.method == 'POST':
-        new_article=Post.objects.create(
-            postname=request.POST['postname'],
-            contents=request.POST['contents'],
-        )
-    return render(request, 'new_post.html')
+        postname=request.POST['postname']
+        contents=request.POST['contents']
+        
+        if postname and contents:
+            new_article = Post.objects.create(
+                postname=postname,
+                contents=contents,
+                # user=request.user
+            )
+            return redirect('blog')
+    else:
+        return render(request, 'new_post.html')
